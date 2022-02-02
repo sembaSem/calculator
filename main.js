f679b8b03ef85c86a82da3176b66acae0a25870c
sembaSem <sem.goncalves@outlook.com>
2022-02-02 16:55:59 +0100

    Equal button works

--- main.js
+++ main.js
@@ -32,13 +32,51 @@ const operate = function(operator, a, b) {
 
 //display digits on button click
 const output = document.getElementById('output');
-const buttons = document.querySelectorAll('button');
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
 
-buttons.forEach(button => {
-    button.addEventListener('click', display);
+digits.forEach(digit => {
+    digit.addEventListener('click', displayDigit);
 });
 
-function display() {
-    let buttonText = this.innerText
+function displayDigit() {
+    let buttonText = this.innerText;
+    output.textContent += buttonText;
     console.log(buttonText);
 }
+
+operators.forEach(operator => {
+    operator.addEventListener('click', displayOperator);
+});
+
+let operand1 = '';
+let operator = '';
+let operand2 = '';
+let a = 0;
+let b = 0;
+
+function displayOperator() {
+    let buttonText = this.innerText;
+    
+    if (buttonText === 'AC') {
+        output.innerText = '';
+    } else if (buttonText === '=') {
+        const operands = output.textContent.split(/\D/);
+        operand2 = operands[1];
+        a = parseInt(operand1);
+        b = parseInt(operand2);
+        output.innerText = operate(operator, a, b);
+        console.log(operate(operator, a, b));
+    } else {
+        operand1 = output.textContent;
+        operator = buttonText;
+        output.textContent += buttonText;
+    }
+    console.log(operator);
+    console.log(operand1);
+    console.log(operand2);
+    console.log(a);
+    console.log(b);
+}
+
+//
